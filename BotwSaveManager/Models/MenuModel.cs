69abfc45cd49d73a93e353d99f034620163f15fd
ArchLeaders <archparliament@gmail.com>
2022-10-30 16:40:52 -0700

    Implement MenuModel

---
+++ BotwSaveManager/Models/MenuModel.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia.MenuFactory.Attributes;
+using Material.Icons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotwSaveManager.Models
+{
+    public class MenuModel
+    {
+        [Menu("Open Save", "_File", "Ctrl + O", Icon = MaterialIconKind.FolderOpenOutline)]
+        public void OpenSave()
+        {
+
+        }
+
+        [Menu("Readme", "_About", Icon = MaterialIconKind.HandshakeOutline)]
+        public void Readme()
+        {
+
+        }
+
+        [Menu("Help", "_About", Icon = MaterialIconKind.Help)]
+        public void Help()
+        {
+
+        }
+    }
+}
