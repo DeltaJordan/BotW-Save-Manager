a46ac48fc9bb7c6af3d1839bc4a4d831437646e0
ArchLeaders <archparliament@gmail.com>
2022-11-01 00:31:51 -0700

    Create `Convert Save` menu item; rename `OpenSave` > `OpenSaveFolder`

--- BotwSaveManager/Models/MenuModel.cs
+++ BotwSaveManager/Models/MenuModel.cs
@@ -2,10 +2,13 @@
 
 using Avalonia.MenuFactory.Attributes;
 using AvaloniaGenerics.Dialogs;
+using BotwSaveManager.Core;
+using BotwSaveManager.Core.Helpers;
 using BotwSaveManager.Views;
 using Material.Icons;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,22 +17,50 @@ namespace BotwSaveManager.Models
 {
     public class MenuModel
     {
-        [Menu("Open Save", "_File", "Ctrl + O", Icon = MaterialIconKind.FolderOpenOutline)]
-        public async void OpenSave()
+        [Menu("Open Save Folder", "_File", "Ctrl + O", Icon = MaterialIconKind.FolderOpenOutline)]
+        public async void OpenSaveFolder()
         {
             string? folder = await BrowserDialog.Folder.ShowDialog("Open Botw Save Folder");
 
             if (folder != null) {
+                try {
+                    ViewModel.BotwSave = new(folder, true);
+                }
+                catch (Exception ex)  {
+                    Logger.Write(ex);
+                    await MessageBox.Show(ex.Message, "Error", icon: MaterialIconKind.FileDocumentErrorOutline);
+                }
+            }
+        }
 
+        [Menu("Convert Save", "_File", "F3", Icon = MaterialIconKind.ContentSaveMoveOutline)]
+        public async void ConvertSave()
+        {
+            string? folder = await BrowserDialog.Folder.ShowDialog("Open Botw Save Folder");
+
+            if (folder != null) {
+                try {
+                    BotwSave save = new(folder, true);
+                    if (await MessageBox.Show($"Identified {save.SaveType} save, convert to {save.SaveType.Reverse()}?", "Notice", MessageBoxButtons.YesNoCancel, icon: MaterialIconKind.ContentSaveMoveOutline) == MessageBoxResult.Yes) {
+                        string? output = await BrowserDialog.Folder.ShowDialog("Open Botw Save Folder");
+                        if (output != null) {
+                            await Task.Run(() => save.ConvertPlatform(output));
+                            await MessageBox.Show($"Save succefully converted!", "Notice", icon: MaterialIconKind.InfoCircleOutline);
+                        }
+
+                    }
+                }
+                catch (Exception ex) {
+                    Logger.Write(ex);
+                    await MessageBox.Show(ex.Message, "Error", icon: MaterialIconKind.FileDocumentErrorOutline);
+                }
             }
         }
 
         [Menu("Quit", "_File", "Alt + F4", Icon = MaterialIconKind.ExitToApp, IsSeparator = true)]
         public async void Quit()
         {
-            if (await MessageBox.Show("Are you sure you wish to exit?", "Warning", MessageBoxButtons.YesNoCancel, icon: MaterialIconKind.WarningBox) == MessageBoxResult.Yes) {
-                Environment.Exit(0);
-            }
+            Environment.Exit(0);
         }
 
         [Menu("Readme", "_About", Icon = MaterialIconKind.HandshakeOutline)]
