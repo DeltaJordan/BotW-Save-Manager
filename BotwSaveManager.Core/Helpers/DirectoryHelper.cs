0eb5489176cfbcbcaa56321339e9c9234753df7d
ArchLeaders <archparliament@gmail.com>
2022-10-31 22:28:23 -0700

    Impl `DirectoryHelper.Copy`

---
+++ BotwSaveManager.Core/Helpers/DirectoryHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace BotwSaveManager.Core.Helpers
+{
+    public class DirectoryHelper
+    {
+        public static void Copy(string src, string dst, bool overwrite = false, string searchPattern = "*.*", SearchOption searchOption = SearchOption.AllDirectories)
+        {
+            Parallel.ForEach(Directory.EnumerateFiles(src, searchPattern, searchOption), (srcFile) => {
+                string dstFile = $"{dst}/{Path.GetRelativePath(src, srcFile)}";
+                Directory.CreateDirectory(Path.GetDirectoryName(dstFile) ?? "");
+                File.Copy(srcFile, dstFile, overwrite);
+            });
+        }
+    }
+}
