7b5155b679840ed38408fd95a8b36656a6156270
ArchLeaders <archparliament@gmail.com>
2022-11-01 11:21:56 -0700

    Impl `AppView` layout binding

--- BotwSaveManager/ViewModels/AppViewModel.cs
+++ BotwSaveManager/ViewModels/AppViewModel.cs
@@ -2,6 +2,8 @@
 
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Layout;
+using BotwSaveManager.Core;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
@@ -14,6 +16,21 @@ namespace BotwSaveManager.ViewModels
 {
     public class AppViewModel : ReactiveObject
     {
+        private static readonly TextBlock Default = new() {
+            Text = "Drag and drop a save folder to open it.",
+            HorizontalAlignment = HorizontalAlignment.Center,
+            VerticalAlignment = VerticalAlignment.Center
+        };
+
+        private object content = Default;
+        public object Content {
+            get => content ?? Default;
+            set => this.RaiseAndSetIfChanged(ref content, value);
+        }
+
+        //
+        // Window Settings
+
         public void ChangeState() => View.WindowState = View.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
         public void Minimize() => View.WindowState = WindowState.Minimized;
         public void Quit() => View.MenuModel.Quit();
