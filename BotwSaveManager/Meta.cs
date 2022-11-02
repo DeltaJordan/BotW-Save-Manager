85376c7f665c17bb38d0cfc5af132253371a5849
ArchLeaders <archparliament@gmail.com>
2022-11-02 02:04:21 -0700

    Release version: `2.0.0`

--- BotwSaveManager/Meta.cs
+++ BotwSaveManager/Meta.cs
@@ -5,7 +5,7 @@ namespace BotwSaveManager
     public static class Meta
     {
         public static string Name { get; } = "Botw Save Manager";
-        public static string Version { get; } = "0.1.0-alpha";
+        public static string Version { get; } = "2.0.0";
         public static string Footer { get; } = $"{Name} - v{Version}";
         public static bool IsWindows { get; } = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
     }
